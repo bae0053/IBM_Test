a7e0364177a8187c8e57622f0d85fe6ede4a16c1
bae0053 <50228287+bae0053@users.noreply.github.com>
2021-03-30 10:22:06 +0900

    Update Authentication.py

--- tone_analyzer/Authentication.py
+++ tone_analyzer/Authentication.py
@@ -2,7 +2,7 @@ import json
 from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('2qAcamy6U5GCsKT3TJUQFmbe11R7WZEpZpJh6m61eyJZ')
+authenticator = IAMAuthenticator('MY_KEY')
 tone_analyzer = ToneAnalyzerV3(
     version='2017-09-21',
     authenticator=authenticator
